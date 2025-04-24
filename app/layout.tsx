7d9433394e96036a5ab42146ffc89c3ab08202e8
CubeStar1 <143164352+CubeStar1@users.noreply.github.com>
2025-04-24 10:10:41 +0530

    Chore: added clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/global/theme-provider"
 import QueryProvider from "@/components/global/query-provider";
 import { Toaster } from "@/components/ui/toaster"
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,6 +29,15 @@ export default function RootLayout({
               disableTransitionOnChange
             >
               {children}
+              <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "r8zk4lkg97");
+          `}
+        </Script> 
             </ThemeProvider>
           </QueryProvider>
         <Toaster />
